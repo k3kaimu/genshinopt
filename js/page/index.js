dfb12ac3e8b6b74eff176425ac43f46ca259c7f2
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-11-13 19:52:52 +0900

    add multi addition modal for weapon

--- js/page/index.js
+++ js/page/index.js
@@ -26,6 +26,70 @@ $(function(){
     }
 
 
+    function MultiWeaponAdderModal(parent)
+    {
+        this.parent = parent;
+        this.selWeaponRarity = ko.observable();
+
+
+        this.weapons = [];
+        genshinData.weapons.forEach(e => {
+            this.weapons.push({
+                weapon: e,
+                checked: ko.observable(false)
+            });
+        });
+
+        this.selWeaponList = ko.computed(function(){
+            var list = [];
+            var rarity = this.selWeaponRarity();
+            var ch = this.parent.selectedChar();
+
+            this.weapons.forEach(e => {
+                if(!(rarity == "ALL" || e.weapon.rarity == rarity))
+                    return;
+
+                if(ch != undefined && ch.weaponType != e.weapon.weaponType)
+                    return;
+
+                list.push(e);
+            });
+
+            return list;
+        }.bind(this));
+
+        this.addWeapons = function(){
+            var removeList = [];
+            this.parent.comparingWeaponList().forEach(e => {
+                if(e.selectedWeapon() == undefined)
+                    removeList.push(e);
+            });
+            
+            removeList.forEach(function(e) { this.parent.comparingWeaponList.remove(e); }.bind(this));
+
+            this.weapons.forEach(e => {
+                if(e.checked()) {
+                    var wp = new ComparingWeaponData(this.parent.selectedChar);
+                    wp.selectedWeapon(e.weapon);
+                    this.parent.comparingWeaponList.push(wp);
+                }
+            });
+        }.bind(this);
+
+        this.uncheckALL = function(){
+            this.weapons.forEach(e => {
+                e.checked(false);
+            });
+        }.bind(this);
+
+        this.checkALL = function() {
+            this.selWeaponList().forEach(e => {
+                e.checked(true);
+            });
+        }.bind(this);
+    }
+
+
     function ViewModel() {
         this.characters = genshinData.characters;
         this.selCharElem = ko.observable();
@@ -64,6 +128,11 @@ $(function(){
             this.comparingWeaponList.push(new ComparingWeaponData(this.selectedChar));
         }.bind(this);
 
+        
+        this.multiWeaponAdderModal = new MultiWeaponAdderModal(this);
+        this.initMultiWeaponAdder = function(){
+            this.multiWeaponAdderModal.uncheckALL();
+        }.bind(this);
 
         this.totalBaseHP = ko.computed(function(){
             return tryAndDefault(() => this.selectedChar().baseHP, 0)
