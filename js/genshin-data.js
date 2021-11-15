87d3aa472eb7a6cdb99efdc70b04910319d5a4af
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-11-16 00:39:00 +0900

    add some artifacts

--- js/genshin-data.js
+++ js/genshin-data.js
@@ -340,6 +340,156 @@ class PrimordialJadeCutter extends WeaponData
     }
 }
 
+
+class ArtifactData
+{
+    constructor(id, name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    newViewModel()
+    {
+        return new ArtifactViewModel(this);
+    }
+}
+
+
+class ArtifactViewModel
+{
+    // bonusType: '2' or '4'
+    constructor(data, bonusType)
+    {
+        this.parent = data;
+        this.bonusType = bonusType;
+    }
+
+
+    viewHTMLList(target)
+    {
+        return [];
+    }
+}
+
+
+// 燃え盛る炎の魔女
+class CrimsonWitchOfFlames extends ArtifactData
+{
+    constructor()
+    {
+        super(
+            'crimson_witch_of_flames',
+            "燃え盛る炎の魔女"
+        )
+    }
+
+
+    newViewModel()
+    {
+        return new CrimsonWitchOfFlamesViewModel(this);
+    }
+}
+
+
+// 燃え盛る炎の魔女, ViewModel
+class CrimsonWitchOfFlamesViewModel extends ArtifactViewModel
+{
+    constructor(parent, bonusType)
+    {
+        super(parent, bonusType);
+        this.buffStacks = ko.observable();
+    }
+
+
+    viewHTMLList(target)
+    {
+        var list = [];
+
+        if(this.bonusType == '4') {
+            list.push(
+                `
+                <div class="card">
+                    <div class="card-header p-2">火魔女：炎ダメバフ</div>
+                    <div class="card-body p-2">
+                        <div class="form-group m-0">
+                            <div class="form-check" data-bind="with: ` + target + `">
+                                <select class="custom-select" aria-label="バフ効果量" data-bind="value: buffStacks">
+                                    <option value="0">+15.0%</option>
+                                    <option value="1">+22.5%</option>
+                                    <option value="2">+30.0%</option>
+                                    <option selected value="3">+37.5%</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `);
+        }
+
+        return list;
+    }
+}
+
+
+// 追憶のしめ縄
+class ShimenawaReminiscence extends ArtifactData
+{
+    constructor()
+    {
+        super(
+            'shimenawa_reminiscence',
+            "追憶のしめ縄"
+        );
+    }
+
+
+    newViewModel()
+    {
+        return new ShimenawaReminiscenceViewModel(this);
+    }
+}
+
+
+// 追憶のしめ縄, ViewModel
+class ShimenawaReminiscenceViewModel extends ArtifactViewModel
+{
+    constructor(parent, bonusType)
+    {
+        super(parent, bonusType);
+        this.buffEffect = ko.observable();
+    }
+
+
+    viewHTMLList(target)
+    {
+        var list = [];
+
+        if(this.bonusType == '4') {
+            list.push(
+                `
+                <div class="card">
+                    <div class="card-header p-2">しめ縄：ダメバフ</div>
+                    <div class="card-body p-2">
+                        <div class="form-group m-0">
+                            <div class="form-check" data-bind="with: ` + target + `">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox" data-bind="checked: buffEffect" checked>
+                                    通常・重撃・落下+50%ダメバフ（元素エネ15消費）
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `);
+        }
+
+        return list;
+    }
+}
+
+
 genshinData = {
     characters: [
         new TravelerAnemo(),
@@ -351,5 +501,9 @@ genshinData = {
     weapons: [
         new StaffOfHoma(),
         new PrimordialJadeCutter(),
-    ]
+    ],
+
+    artifacts: [
+        new ShimenawaReminiscence(),
+    ],
 };
