7e55db7cb610011212da66caab1d10a45832d9c0
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-11-18 00:40:50 +0900

    add unittest

--- js/unittest.js
+++ js/unittest.js
@@ -61,10 +61,13 @@ $(function(){
     var calc = new DamageCalculator();
     calc.baseAtk = calc.baseAtk.add(821);
     calc.addAtk = calc.addAtk.add(379);
-    calc.rateAtk = calc.rateAtk.add(0.24).add(VGData.newRateAtk(0.25));
-    calc.baseMastery = calc.baseMastery.add(VGData.newMastery(434));
-    calc.baseCrtRate = calc.baseCrtRate.add(0.09).add(VGData.newCrtRate(0.165));
-    calc.baseCrtDmg = calc.baseCrtDmg.add(0.5).add(VGData.newCrtDmg(0.33));
+    calc.rateAtk = calc.rateAtk.add(0.24);
+    calc.artRateAtk.value = 0.25;
+    calc.artMastery.value = 434;
+    calc.baseCrtRate = calc.baseCrtRate.add(0.09);
+    calc.artCrtRate.value = 0.165;
+    calc.baseCrtDmg = calc.baseCrtDmg.add(0.5);
+    calc.artCrtDmg.value = 0.33;
 
     var dmg = calc.calculateDmg(0.817, {}).mul(0.5).mul(0.9);
     console.assert(Math.round(dmg.value) == 714, dmg);
@@ -88,4 +91,107 @@ $(function(){
     console.assert(Math.round(dmg.grad[4]) == 0);
     console.assert(Math.round(dmg.grad[5]) == 0);
     console.assert(Math.round(dmg.grad[6]*100) == 539);
+});
+
+
+
+$(function(){
+    class HutaoDamageCalculator extends DamageCalculator
+    {
+        constructor() { super(); }
+        
+        atk() {
+            return super.atk().add(super.hp().mul(0.0566 + 0.018));
+        }
+    }
+
+    
+    var calc = new HutaoDamageCalculator();
+    calc.baseAtk = VGData.constant(711);
+    calc.addAtk = VGData.constant(311);
+    calc.baseDef = VGData.constant(845);
+    calc.baseHP = VGData.constant(15004);
+    calc.addHP = VGData.constant(4780);
+    calc.rateHP = VGData.constant(0.466 + 0.2);
+    calc.basePyroDmg = VGData.constant(0.466 + 0.33);
+    calc.baseCrtRate = VGData.constant(0.05 + 0.311);
+    calc.baseCrtDmg = VGData.constant(1.543);
+
+
+    function objfunc(x) {
+        calc.artRateAtk.value = x[0];
+        calc.artRateDef.value = x[1];
+        calc.artRateHP.value = x[2];
+        calc.artCrtRate.value = x[3];
+        calc.artCrtDmg.value = x[4];
+        calc.artRecharge.value = x[5];
+        calc.artMastery.value = x[6];
+
+        return calc.calculateDmg(1, {isPyro: true});
+    }
+
+
+    (async () => {
+        await nlopt.ready
+
+        function applyOptimize(algoritm, x0, tol, maxEval) {
+            const opt = new nlopt.Optimize(algoritm, 7);
+
+            opt.setMaxObjective((x, grad) => {
+                var dmg = objfunc(x);
+
+                if(grad) {
+                    for(let i = 0; i < 7; ++i)
+                        grad[i] = dmg.grad[i];
+                }
+
+                return dmg.value;
+            }, tol);
+
+            
+            opt.addInequalityConstraint((x, grad) => {
+                var cost = calcSubOptionCost(x);
+                if(grad) {
+                    for(let i = 0; i < 7; ++i)
+                        grad[i] = cost.grad[i];
+                }
+
+                return cost.value - 25*5;
+            }, tol);
+
+            opt.setUpperBounds(calcUpperBounds(25*5, objfunc));
+            opt.setLowerBounds([0, 0, 0, 0, 0, 0, 0]);
+            opt.setMaxeval(maxEval);
+
+            return opt.optimize(x0);
+        }
+
+        console.log(calcUpperBounds(25*5, objfunc));
+
+        for(let i = 0; i < 1; ++i) {
+            try {
+                const startTime = performance.now();
+                const res1 = applyOptimize(nlopt.Algorithm.GN_ISRES, [0, 0, 0, 0, 0, 0, 0], 1e-2, 1000);
+                const endTime1 = performance.now();
+                const res2 = applyOptimize(nlopt.Algorithm.LD_SLSQP, res1.x, 1e-3, 1e6);
+                const endTime2 = performance.now();
+                // const res2 = applyOptimize(nlopt.Algorithm.LD_SLSQP, [0, 0, 0, 0, 0, 0, 0], 1e-4); 
+
+                console.log(res2);
+                // console.log(calcSubOptionCost(res.x));
+                console.log(objfunc(res2.x));
+                // console.log(calc.atk());
+                // console.log(calc.hp());
+                console.log(calc.crtRate());
+                console.log(calc.crtDmg());
+                console.log(endTime1 - startTime);
+                console.log(endTime2 - endTime1);
+
+                // Flush the GC
+                nlopt.GC.flush();
+            } catch(ex) {
+
+            }
+        }
+      })();
 });
