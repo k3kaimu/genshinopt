362da1652d4b765067589f12e503d55dfb239dc8
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-12-04 20:27:27 +0900

    update PrototypeArchaic

--- js/modules/weapons/claymore.mjs
+++ js/modules/weapons/claymore.mjs
@@ -1,3 +1,4 @@
+import * as Calc from '/js/modules/dmg-calc.mjs'
 import * as Base from '/js/modules/weapons/base.mjs'
 
 
@@ -104,5 +105,114 @@ export class PrototypeArchaic extends Base.WeaponData
             "rateAtk",
             0.276
         );
+    }
+
+
+    newViewModel()
+    {
+        return new PrototypeArchaicViewModel(this);
+    }
+
+
+    static additionalScale = [2.4, 3.0, 3.6, 4.2, 4.8];
+}
+
+
+
+// 古華・試作, viewmodel
+export class PrototypeArchaicViewModel extends Base.WeaponViewModel
+{
+    constructor(parent)
+    {
+        super(parent);
+        this.useEffect = ko.observable(false);
+        this.perAttack = ko.observable(10);
+    }
+
+
+    applyDmgCalc(calc)
+    {
+        calc = super.applyDmgCalc(calc);
+
+        if(! this.useEffect())
+            return calc;
+
+        let CalcType = Object.getPrototypeOf(calc).constructor;
+        let scale = PrototypeArchaic.additionalScale[this.rank()];
+        let perAttack_ = Number(this.perAttack());
+
+        let NewCalc = class extends CalcType {
+            scaleOfPrototypeArchaic = scale;
+            perAttackOfPrototypeArchaic = perAttack_;
+
+            chainedAttackDmg(attackProps) {
+                let superValue = super.chainedAttackDmg(attackProps);
+
+                if(hasAnyPropertiesWithSameValue(attackProps, {isNormal: true, isCharged: true})) {
+                    let newProps = shallowDup(attackProps);
+                    // 元々の攻撃の属性や攻撃種類を削除する
+                    newProps = Calc.deleteAllElementFromAttackProps(attackProps);
+                    newProps = Calc.deleteAllAttackTypeFromAttackProps(attackProps);
+
+                    newProps.isPhysical = true;   // 物理攻撃
+                    newProps.isChainable = false; // この攻撃では追撃は発生しない
+                    return superValue.add(super.calculateDmg(this.scaleOfPrototypeArchaic, newProps).div(this.perAttackOfPrototypeArchaic));
+                } else {
+                    // 通常攻撃でも重撃でもないので，追撃は発生しない
+                    return superValue;
+                }
+            }
+        };
+
+        calc = Object.assign(new NewCalc(), calc);
+        return calc;
+    }
+
+
+    addAttackPercentageText()
+    {
+        return textPercentageFix(PrototypeArchaic.additionalScale[this.rank()], 0);
+    }
+
+
+    viewHTMLList(target)
+    {
+        let dst = super.viewHTMLList(target);
+
+        dst.push(`
+        <div class="card" data-bind="with: ${target}">
+            <div class="card-header p-2">通常/重撃時：追加ダメージ</div>
+            <div class="card-body p-2">
+                <div class="form-check" data-bind="with: ` + target + `">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" data-bind="checked: useEffect">
+                        <span data-bind="text: perAttack"></span>回に1回<span data-bind="text: addAttackPercentageText()"></span>物理ダメージ
+                    </label>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-5 col-form-label">発生頻度</label>
+                    <div class="col-sm-7 mt-sm-2">
+                    <input type="range" data-bind="value: perAttack, disable: !useEffect()" class="form-control-range" min="1" max="20" step="1">
+                    </div>
+                </div>
+            </div>
+        </div>
+        `);
+
+        return dst;
+    }
+
+
+    toJS() {
+        let obj = super.toJS();
+        obj.perAttack = this.perAttack();
+
+        return obj;
+    }
+
+
+    fromJS(obj) {
+        super.fromJS(obj);
+        this.perAttack(obj.perAttack);
     }
 }
