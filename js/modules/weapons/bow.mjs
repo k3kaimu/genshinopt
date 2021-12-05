2fcf213f9202b93dab0f86cee88213d4eac12f5f
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-12-05 17:22:34 +0900

    add SkywardHarp

--- js/modules/weapons/bow.mjs
+++ js/modules/weapons/bow.mjs
@@ -1,1 +1,114 @@
-import * as Base from '/js/modules/weapons/base.mjs'
+import * as Base from '/js/modules/weapons/base.mjs';
+import * as Calc from '/js/modules/dmg-calc.mjs';
+import * as Widget from '/js/modules/widget.mjs';
+
+
+
+// 天空の翼
+export class SkywardHarp extends Base.WeaponData
+{
+    constructor()
+    {
+        super(
+            "skyward_harp",
+            "天空の翼",
+            5,
+            "Bow",
+            674,
+            "baseCrtRate",
+            0.221
+        );
+    }
+
+
+    newViewModel()
+    {
+        return new SkywardHarpViewModel(this);
+    }
+
+
+    static addCrtDmg = [0.2, 0.25, 0.30, 0.35, 0.40];
+}
+
+
+
+// 天空の翼, viewmodel
+export class SkywardHarpViewModel extends Base.WeaponViewModel
+{
+    constructor(parent)
+    {
+        super(parent);
+        this.useEffect = ko.observable(false);
+        this.perAttack = ko.observable(5);
+    }
+
+
+    applyDmgCalc(calc)
+    {
+        calc = super.applyDmgCalc(calc);
+
+        calc.baseCrtDmg.value += SkywardHarp.addCrtDmg[this.rank()];
+
+        if(! this.useEffect())
+            return calc;
+
+        let CalcType = Object.getPrototypeOf(calc).constructor;
+        let perAttack_ = Number(this.perAttack());
+
+        let NewCalc = class extends CalcType {
+            perAttackOfSkywardHarp = perAttack_;
+
+            chainedAttackDmg(attackProps) {
+                let superValue = super.chainedAttackDmg(attackProps);
+
+                if(hasAnyPropertiesWithSameValue(attackProps, {isNormal: true, isCharged: true})) {
+                    let newProps = shallowDup(attackProps);
+                    // 元々の攻撃の属性や攻撃種類を削除する
+                    newProps = Calc.deleteAllElementFromAttackProps(attackProps);
+                    newProps = Calc.deleteAllAttackTypeFromAttackProps(attackProps);
+
+                    newProps.isPhysical = true;   // 物理攻撃
+                    newProps.isChainable = false; // この攻撃では追撃は発生しない
+                    return superValue.add(super.calculateNormalDmg(1.25, newProps).div(this.perAttackOfSkywardHarp));
+                } else {
+                    // 通常攻撃でも重撃でもないので，追撃は発生しない
+                    return superValue;
+                }
+            }
+        };
+
+        calc = Object.assign(new NewCalc(), calc);
+        return calc;
+    }
+
+
+    viewHTMLList(target)
+    {
+        let dst = super.viewHTMLList(target);
+
+        dst.push(
+            Widget.buildViewHTML(target, "追加ダメージ",
+                Widget.checkBoxViewHTML("useEffect",
+                    `${Widget.spanText("perAttack()")}回に1回125%物理ダメージ`)
+                +
+                Widget.sliderViewHTML("perAttack", 1, 20, 1, `発生頻度`, {disable: "!useEffect()"})
+            )
+        );
+
+        return dst;
+    }
+
+
+    toJS() {
+        let obj = super.toJS();
+        obj.perAttack = this.perAttack();
+
+        return obj;
+    }
+
+
+    fromJS(obj) {
+        super.fromJS(obj);
+        this.perAttack(obj.perAttack);
+    }
+}
