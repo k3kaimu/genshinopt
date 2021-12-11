6a8339f961a8c20e3e5b47881f155337f471f631
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-12-11 17:51:39 +0900

    update PrimordialJadeCutter

--- js/modules/weapons/sword.mjs
+++ js/modules/weapons/sword.mjs
@@ -16,4 +16,47 @@ export class PrimordialJadeCutter extends Base.WeaponData
             0.441
         );
     }
+
+
+    newViewModel()
+    {
+        return new PrimordialJadeCutterViewModel(this);
+    }
+
+
+    static hpInc = [0.20, 0.25, 0.30, 0.35, 0.40];
+    static atkInc = [0.012, 0.015, 0.018, 0.021, 0.024];
 }
+
+
+// 磐岩結緑
+export class PrimordialJadeCutterViewModel extends Base.WeaponViewModel
+{
+    constructor(parent)
+    {
+        super(parent);
+    }
+
+
+    applyDmgCalc(calc)
+    {
+        calc = super.applyDmgCalc(calc);
+
+        let data = this.toJS();
+        data.atkInc = PrimordialJadeCutter.atkInc[data.rank];
+        calc.rateHP.value += PrimordialJadeCutter.hpInc[data.rank];
+
+        let CalcType = Object.getPrototypeOf(calc).constructor;
+        let NewCalc = class extends CalcType {
+            priJadeCutData = data;
+
+            atk(attackProps) {
+                return super.atk(attackProps).add(this.hp(attackProps).mul(this.priJadeCutData.atkInc));
+            }
+        };
+
+        calc = Object.assign(new NewCalc(), calc);
+        return calc;
+    }
+}
+
