946192c0af755a5d26588c45b9a82af631aa1e71
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-12-13 23:42:35 +0900

    add some utilities

--- js/main.js
+++ js/main.js
@@ -284,3 +284,67 @@ runUnittest(function(){
     console.assert(hasAnyPropertiesWithSameValue(obj, {a:1}) === false);
     console.assert(hasAnyPropertiesWithSameValue(obj, {d:1}) === false);
 });
+
+
+function uint8ArrayToBase64(data)
+{
+    return btoa(String.fromCharCode(...new Uint8Array(data)));
+}
+
+
+function base64ToUint8Array(str)
+{
+    return Uint8Array.from(atob(str), c => c.charCodeAt(0));
+}
+
+
+function encodeToURI(obj)
+{
+    return encodeURIComponent(uint8ArrayToBase64(LZMA.compress(MessagePack.encode(obj))).replace('+', '-').replace('/', '_'));
+}
+
+
+function decodeFromURI(uri)
+{
+    return MessagePack.decode(LZMA.decompress(base64ToUint8Array(decodeURIComponent(uri).replace('-', '+').replace('_', '/'))));
+}
+
+
+// https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+    //   console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+}
+
+
+//   https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        // console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
