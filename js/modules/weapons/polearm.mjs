8bce87cb16ac825201fa76094a1011edba5d0715
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-12-28 11:25:44 +0900

    sort polearmz

--- js/modules/weapons/polearm.mjs
+++ js/modules/weapons/polearm.mjs
@@ -313,91 +313,6 @@ export class VortexVanquisherViewModel extends Base.WeaponViewModel
 }
 
 
-
-// 匣中滅龍
-export class DragonsBane extends Base.WeaponData
-{
-    constructor()
-    {
-        super(
-            "dragons_bane",
-            "匣中滅龍",
-            4,
-            "Polearm",
-            454,
-            "baseMastery",
-            221
-        );
-    }
-
-
-    newViewModel()
-    {
-        return new DragonsBaneViewModel(this);
-    }
-
-
-    static dmgBuff = [0.2, 0.24, 0.28, 0.32, 0.36];
-}
-
-
-// 匣中滅龍
-export class DragonsBaneViewModel extends Base.WeaponViewModel
-{
-    constructor(parent)
-    {
-        super(parent);
-        this.useEffect = ko.observable(true);
-    }
-
-
-    applyDmgCalc(calc)
-    {
-        calc = super.applyDmgCalc(calc);
-
-        if(this.useEffect()) {
-            calc.baseAllDmg.value += DragonsBane.dmgBuff[this.rank()];
-        }
-
-        return calc;
-    }
-
-
-    textDmgBuff() {
-        return textPercentage(DragonsBane.dmgBuff[this.rank()], 3);
-    }
-
-
-    viewHTMLList(target)
-    {
-        let dst = super.viewHTMLList(target);
-
-        dst.push(
-            Widget.buildViewHTML(target, "炎と水の破滅",
-                Widget.checkBoxViewHTML("useEffect", 
-                    `水/炎の影響を受けた敵へのダメージ+` + Widget.spanText("textDmgBuff()") )
-            )
-        );
-
-        return dst;
-    }
-
-
-    toJS() {
-        let obj = super.toJS();
-        obj.useEffect = this.useEffect();
-
-        return obj;
-    }
-
-
-    fromJS(obj) {
-        super.fromJS(obj);
-        this.useEffect(obj.useEffect);
-    }
-}
-
-
 // 護摩の杖
 export class StaffOfHoma extends Base.WeaponData
 {
@@ -617,6 +532,90 @@ export class FavoniusLance extends Base.WeaponData
             "baseRecharge",
             0.306
         );
+    }
+}
+
+
+// 匣中滅龍
+export class DragonsBane extends Base.WeaponData
+{
+    constructor()
+    {
+        super(
+            "dragons_bane",
+            "匣中滅龍",
+            4,
+            "Polearm",
+            454,
+            "baseMastery",
+            221
+        );
+    }
+
+
+    newViewModel()
+    {
+        return new DragonsBaneViewModel(this);
+    }
+
+
+    static dmgBuff = [0.2, 0.24, 0.28, 0.32, 0.36];
+}
+
+
+// 匣中滅龍
+export class DragonsBaneViewModel extends Base.WeaponViewModel
+{
+    constructor(parent)
+    {
+        super(parent);
+        this.useEffect = ko.observable(true);
+    }
+
+
+    applyDmgCalc(calc)
+    {
+        calc = super.applyDmgCalc(calc);
+
+        if(this.useEffect()) {
+            calc.baseAllDmg.value += DragonsBane.dmgBuff[this.rank()];
+        }
+
+        return calc;
+    }
+
+
+    textDmgBuff() {
+        return textPercentage(DragonsBane.dmgBuff[this.rank()], 3);
+    }
+
+
+    viewHTMLList(target)
+    {
+        let dst = super.viewHTMLList(target);
+
+        dst.push(
+            Widget.buildViewHTML(target, "炎と水の破滅",
+                Widget.checkBoxViewHTML("useEffect", 
+                    `水/炎の影響を受けた敵へのダメージ+` + Widget.spanText("textDmgBuff()") )
+            )
+        );
+
+        return dst;
+    }
+
+
+    toJS() {
+        let obj = super.toJS();
+        obj.useEffect = this.useEffect();
+
+        return obj;
+    }
+
+
+    fromJS(obj) {
+        super.fromJS(obj);
+        this.useEffect(obj.useEffect);
     }
 }
 
