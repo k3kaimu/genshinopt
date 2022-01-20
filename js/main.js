c483688ff7a4319496b063fd526f5a2ad0a5b75c
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2022-01-20 19:32:09 +0900

    add deepDup and isShallowEqualObject

--- js/main.js
+++ js/main.js
@@ -221,6 +221,29 @@ function shallowDup(obj)
 }
 
 
+function deepDup(obj)
+{
+    return MessagePack.decode(MessagePack.encode(obj));
+}
+
+
+function isShallowEqualObject(obj1, obj2) {
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+
+    if(keys1.length !== keys2.length)
+        return false;
+
+    for(let key of keys1){
+        if(obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 
 /**
  * @param {Object} obj
