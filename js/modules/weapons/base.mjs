2d7a65ab056992925751db27284fdbc4c0e3e920
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2022-01-28 00:54:33 +0900

    add class WeaponBaseATK

--- js/modules/weapons/base.mjs
+++ js/modules/weapons/base.mjs
@@ -1,5 +1,6 @@
 import * as Calc from '../dmg-calc.mjs';
 import * as Widget from '../widget.mjs';
+import * as Utils from '../utils.mjs';
 
 
 export class WeaponData
@@ -255,3 +256,128 @@ export class TestWeapon extends WeaponData
         );
     }
 }
+
+
+/**
+ * 武器の基礎攻撃力を線形補間して計算するためのクラス
+ */
+class WeaponBaseATK
+{
+    /**
+     * Lv90時の基礎攻撃力から，各レベルの攻撃力を計算します．
+     * 星2と星1武器はレベル90の基礎攻撃力の代わりにレベル70時点の値を指定します．
+     * @param {number} rarity 
+     * @param {number} baseATKLv90 
+     */
+    constructor(rarity, baseATKLv90)
+    {
+        this.levelBaseATKTable = null;
+        this.rankBaseATKTable = null;
+
+        if(rarity == 5)
+        {
+            this.rankBaseATKTable = [0, 31.1, 62.2, 93.4, 124.5, 155.6, 186.7];
+
+            switch(baseATKLv90) {
+                case 741:
+                    this.levelBaseATKTable = [49, 145, 286, 374, 464, 555, 648, 741];
+                    break;
+                case 674:
+                    this.levelBaseATKTable = [48, 133, 261, 341, 423, 506, 590, 674];
+                    break;
+                case 608:
+                    this.levelBaseATKTable = [46, 122, 235, 308, 382, 457, 532, 608];
+                    break;
+                case 542:
+                    this.levelBaseATKTable = [44, 110, 210, 275, 341, 408, 475, 542];
+                    break;
+                default:
+                    console.assert(false, `Illegal baseATKLv90=${baseATKLv90}`);
+            }
+        }
+        else if(rarity == 4)
+        {
+            this.rankBaseATKTable = [0, 25.9, 51.9, 77.8, 103.7, 129.7, 155.6];
+
+            switch(baseATKLv90) {
+                case 620:
+                    this.levelBaseATKTable = [45, 128, 247, 321, 395, 470, 545, 620];
+                    break;
+                case 565:
+                    this.levelBaseATKTable = [44, 119, 226, 293, 361, 429, 497, 565];
+                    break;
+                case 510:
+                    this.levelBaseATKTable = [42, 109, 205, 266, 327, 388, 449, 510];
+                    break;
+                case 454:
+                    this.levelBaseATKTable = [41, 99, 184, 238, 293, 347, 401, 454];
+                    break;
+                case 440:
+                    this.levelBaseATKTable = [39, 94, 176, 229, 282, 335, 388, 440];
+                    break;
+                default:
+                    console.assert(false, `Illegal baseATKLv90=${baseATKLv90}`);
+            }
+        }
+        else if(rarity == 3)
+        {
+            this.rankBaseATKTable = [0, 19.5, 38.9, 58.4, 77.8, 97.3, 116.7];
+
+            switch(baseATKLv90){
+                case 448:
+                    this.levelBaseATKTable = [40, 102, 187, 239, 292, 344, 396, 448];
+                    break;
+                case 401:
+                    this.levelBaseATKTable = [39, 94, 169, 216, 263, 309, 355, 401];
+                    break;
+                case 354:
+                    this.levelBaseATKTable = [38, 86, 151, 193, 234, 274, 314, 354];
+                    break;
+                default:
+                    console.assert(false, `Illegal baseATKLv90=${baseATKLv90}`);
+            }
+        }
+        else if(rarity == 2)
+        {
+            this.rankBaseATKTable = [0, 11.7, 23.3, 35.0, 46.7];
+            this.levelBaseATKTable = [33, 80, 139, 174, 209, 243];
+            console.assert(baseATKLv90 === 243, `Illegal baseATKLv90=${baseATKLv90}`);
+        }
+        else
+        {
+            this.rankBaseATKTable = [0, 11.7, 23.3, 35.0, 46.7];
+            this.levelBaseATKTable = [23, 56, 102, 130, 158, 185];
+            console.assert(baseATKLv90 === 185, `Illegal baseATKLv90=${baseATKLv90}`);
+        }
+    }
+
+
+    atLv(strLv)
+    {
+        const levelMap = [1, 20, 40, 50, 60, 70, 80, 90];
+        const parsedLv = Utils.parseStrLevel(strLv);
+        // 突破rankが r のとき，this.levelBaseATKTable[r] + <突破による増加値> から this.levelBaseATKTable[r+1] を両端に線形補間すればよい
+
+        const start = this.levelBaseATKTable[parsedLv.rank] + (parsedLv.rank == 0 ? 0 : this.rankBaseATKTable[parsedLv.rank] - this.rankBaseATKTable[parsedLv.rank - 1]);
+        const end   = this.levelBaseATKTable[parsedLv.rank + 1];
+
+        const startLv   = levelMap[parsedLv.rank];
+        const endLv     = levelMap[parsedLv.rank + 1];
+
+        // 傾き
+        const scale = (end - start) / (endLv - startLv);
+
+        // 線形補間
+        return start + scale * (parsedLv.level - startLv);
+    }
+}
+
+runUnittest(function(){
+    let obj = new WeaponBaseATK(5, 741);
+    console.assert(isApproxEqual(obj.atLv("90"), 741, 0.4, 1e-3));
+    console.assert(isApproxEqual(obj.atLv("85"), 710, 0.4, 1e-3));
+    console.assert(isApproxEqual(obj.atLv("80+"), 679, 0.4, 1e-3));
+    console.assert(isApproxEqual(obj.atLv("80"), 648, 0.4, 1e-3));
+    console.assert(isApproxEqual(obj.atLv("70+"), 586, 0.4, 1e-3));
+    console.assert(isApproxEqual(obj.atLv("70"), 555, 0.4, 1e-3));
+});
