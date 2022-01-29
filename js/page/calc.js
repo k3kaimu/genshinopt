2393b29c6852de712a4c374f426b1f12dd3facce
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2022-01-29 17:04:00 +0900

    update calc.html

--- js/page/calc.js
+++ js/page/calc.js
@@ -5,105 +5,101 @@ import * as Migrator from '../modules/migrator.mjs'
 import * as UI from '../modules/ui.mjs'
 
 
-class ArtifactStatus
+function newBundleSetting()
+{
+    return new UI.BundleSetting({
+        bCharPicker: true,
+        selectedChar: undefined,
+        bTalent: true,
+        bAttack: false,
+        bWeapon: true,
+        bArtifact: true,
+        bArtifactStatus: true,
+        bExternalBuff: true
+    });
+}
+
+
+class CalcSetting
+{
+    constructor(bundleData)
+    {
+        this.setting = newBundleSetting();
+        this.setting.fromJS(bundleData);
+        this.calc = new Calc.DamageCalculator();
+
+        // 設定をダメージ計算式に反映する
+        this.calc = this.setting.applyDmgCalc(this.calc);
+
+        this.normalAttacks = [];
+        this.skillAttacks = [];
+        this.burstAttacks = [];
+        this.otherAttacks = [];
+
+        // そのキャラクターのすべてのダメージの計算をする
+        let cvm = this.setting.characterVMSetting.viewModel();
+        let attacks = cvm.presetAttacks();
+        attacks.forEach(attack => {
+            let obj = {}
+            obj.id = attack.id;
+            obj.label = attack.label;
+            obj.calc = this.calc;
+            obj.attack = attack;
+            obj.dmg = obj.attack.evaluate(this.calc);
+
+            let pushed = false;
+            if(attack.attackInfos(this.calc).map(a => a.props.isNormal || a.props.isCharged || a.props.isPlunge || false).reduce((a, b) => a || b, false)) {
+                pushed = true;
+                this.normalAttacks.push(obj);
+            }
+
+            if(attack.attackInfos(this.calc).map(a => a.props.isSkill || false).reduce((a, b) => a || b, false)) {
+                pushed = true;
+                this.skillAttacks.push(obj);
+            }
+
+            if(attack.attackInfos(this.calc).map(a => a.props.isBurst || false).reduce((a, b) => a || b, false)) {
+                pushed = true;
+                this.burstAttacks.push(obj);
+            }
+
+            if(!pushed) {
+                this.otherAttacks.push(obj);
+            }
+        });
+    }
+}
+
+
+class ViewModel
 {
     constructor()
     {
-        this.hp = ko.observable(0);
-        this.atk = ko.observable(0);
-        this.def = ko.observable(0);
-        this.mastery = ko.observable(0);
-        this.crtRate = ko.observable(0);
-        this.crtDmg = ko.observable(0);
-        this.healingBonus = ko.observable(0);
-        this.recharge = ko.observable(0);
-        this.shieldBonus = ko.observable(0);
-        this.pyroDmg = ko.observable(0);
-        this.hydroDmg = ko.observable(0);
-        this.dendroDmg = ko.observable(0);
-        this.electroDmg = ko.observable(0);
-        this.anemoDmg = ko.observable(0);
-        this.cryoDmg = ko.observable(0);
-        this.geoDmg = ko.observable(0);
-        this.physicalDmg = ko.observable(0);
-    }
-
-
-    static getValue(v)
-    {
-        let x = Number(v);
-        if(isNaN(x))
-            return 0;
-        else
-            return x;
-    }
-
-
-    /**
-     * @param {Calc.DamageCalculator} calc 
-     */
-    applyDmgCalc(calc)
-    {
-        calc.addHP.value += ArtifactStatus.getValue(this.hp());
-        calc.addAtk.value += ArtifactStatus.getValue(this.atk());
-        calc.addDef.value += ArtifactStatus.getValue(this.def());
-        calc.baseMastery.value += ArtifactStatus.getValue(this.mastery());
-        calc.baseCrtRate.value += ArtifactStatus.getValue(this.crtRate());
-        calc.baseCrtDmg.value += ArtifactStatus.getValue(this.crtDmg());
-        calc.baseHealingBonus.value += ArtifactStatus.getValue(this.healingBonus());
-        calc.baseRecharge.value += ArtifactStatus.getValue(this.recharge());
-        calc.baseRateShieldStrength.value += ArtifactStatus.getValue(this.shieldBonus());
-        calc.basePyroDmg.value += ArtifactStatus.getValue(this.pyroDmg());
-        calc.baseHydroDmg.value += ArtifactStatus.getValue(this.hydroDmg());
-        calc.baseDendroDmg.value += ArtifactStatus.getValue(this.dendroDmg());
-        calc.baseElectroDmg.value += ArtifactStatus.getValue(this.electroDmg());
-        calc.baseAnemoDmg.value += ArtifactStatus.getValue(this.anemoDmg());
-        calc.baseCryoDmg.value += ArtifactStatus.getValue(this.cryoDmg());
-        calc.baseGeoDmg.value += ArtifactStatus.getValue(this.geoDmg());
-        calc.basePhysicalDmg.value += ArtifactStatus.getValue(this.physicalDmg());
-
-        return calc;
-    }
-}
-
-
-class CalcResult
-{
-    constructor(setting, calc)
-    {
-        this.attack = setting.bundle.attackSetting.makeAttackEvaluator(setting.bundle.characterVMSetting.viewModel());
-        this.calc = calc;
-    }
-}
-
-
-$(function(){
-    function ViewModel()
-    {
-        this.bundleSetting = new UI.BundleSetting({
-            bCharPicker: true,
-            selectedChar: undefined,
-            bTalent: true,
-            bAttack: false,
-            bWeapon: true,
-            bArtifact: true,
-            bExternalBuff: true
-        });
-        this.artifactStatus = new ArtifactStatus();
+        this.bundleSetting = newBundleSetting();
 
         this.calculateResult = ko.computed(function(){
             if(!this.bundleSetting.isValid())
                 return undefined;
 
-            let calc = new Calc.DamageCalculator();
-            calc = this.bundleSetting.applyDmgCalc(calc);
-            calc = this.artifactStatus.applyDmgCalc(calc);
-            return new CalcResult({bundle: this.bundleSetting, artifact: this.artifactStatus}, calc);
+            return new CalcSetting(this.bundleSetting.toJS());
         }, this);
     }
 
 
-    window.viewModel = new ViewModel();
+    fromJS(obj) {
+        this.bundleSetting.fromJS(obj);
+    }
+
+
+    toJS() {
+        return this.bundleSetting.toJS();
+    }
+}
+
+
+$(function(){
+    let viewModel = new ViewModel()
+    window.viewModel = viewModel;
     ko.applyBindings(window.viewModel);
 
     function loadDataFromURI(version, uri)
